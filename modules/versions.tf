1f7a62424bb6e851c82eeb370b13ba12740ea2ae
rayanki0206 <121743337+rayanki0206@users.noreply.github.com>
2024-05-27 14:39:46 +0530

    Update versions.tf

--- modules/versions.tf
+++ modules/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = ">=1.44.0"
+      version = ">=1.28.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
